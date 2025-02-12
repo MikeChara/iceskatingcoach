d90a74e82a9f6c56b15975377996c0ecff021c9a
MikeChara <mikejchara@gmail.com>
2025-02-12 02:08:53 +0000

    Comment out unused bit.

--- ice-skating/src/Components/NavButtons.js
+++ ice-skating/src/Components/NavButtons.js
@@ -17,7 +17,7 @@ const NavButtons = ({ scrollToSection }) => {
         <button onClick={() => scrollToSection("new")}>New</button>
         <button onClick={() => scrollToSection("coaching")}>Coaching</button>
         <button onClick={() => scrollToSection("bookings")}>Bookings</button>
-        <button onClick={() => scrollToSection("contact")}>Contact</button>
+        {/* <button onClick={() => scrollToSection("contact")}>Contact</button> */}
       </div>
     </nav>
   );
